a3f8f92853ba6bb3108f75c780d3f1f3ed320b5f
Miron Dima <mirontsov2098@mail.ru>
2023-04-07 00:24:48 +0300

    add

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('hu');
+const select = document.querySelector('select')
+const result = document.getElementById('result')
+
+const render = (cars) => {
+	result.innerHTML = select.options[select.selectedIndex].text
+}
+
+const filter = (cars, selectValue) => {
+	let objCars = cars.filter(carElem => {
+		if (selectValue === carElem.brand) {
+			result.innerHTML = `Тачка ${carElem.brand} ${carElem.model}<br> Цена: ${carElem.price}`
+		}
+	})
+	return objCars
+}
+select.addEventListener('change', (e) => {
+	const value = e.target.value
+	getData('./db/cars.json').then(data => render(filter(data, value)))
+
+})
+
+const getData = async (url) => {
+	const responseCars = await fetch(url)
+	const cars = await responseCars.json()
+	const arrCars = cars.cars
+	return arrCars
+}
+
+getData('./db/cars.json')
+	.then(data => render(data))
+	.catch(error => console.log(error))
